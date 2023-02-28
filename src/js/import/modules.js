972899d4ad019460c4ddf526ad28b1e4a3ea6b2b
ZipLockRTTS <nikkkk01@mail.ru>
2023-02-28 16:52:42 +0700

    tex section and 2 phones section

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,3 +2,4 @@ import "%modules%/header/header";
 import "%modules%/footer/footer";
 import "%modules%/main/_section/main_section";
 import "%modules%/beta/beta";
+import "%modules%/appletext/appletext";
