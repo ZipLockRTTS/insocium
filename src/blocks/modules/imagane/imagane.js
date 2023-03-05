56f62533758a63880cfbe3cbdf3445531ed329e0
ZipLockRTTS <nikkkk01@mail.ru>
2023-03-05 15:31:53 +0700

    running numbers js

--- src/blocks/modules/imagane/imagane.js
+++ src/blocks/modules/imagane/imagane.js
@@ -1,1 +1,18 @@
 AOS.init();
+
+var time = 2;
+  $('.number').each(function() {
+          var
+            i = 1,
+            num = $(this).data('num'),
+            step = 1000 * time / num,
+            that = $(this),
+            int = setInterval(function() {
+              if (i <= num) {
+                that.html(i);
+              } else {
+                clearInterval(int);
+              }
+              i++;
+            }, step);
+});
