8c51196bfa08bcf1c4319052b6ae7085afc172b7
ZipLockRTTS <nikkkk01@mail.ru>
2023-03-07 20:12:56 +0700

    rework applesection and progress card's

--- src/blocks/modules/appletext/appletext.js
+++ src/blocks/modules/appletext/appletext.js
@@ -1,16 +1,53 @@
-function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-       change.target.classList.add('element-show');
-      }
+let p = window.innerHeight/2;
+let n = window.innerHeight/2.5;
+let elemAnimation1 = document.querySelector('.test_title1');
+let elemAnimation2 = document.querySelector('.test_title2');
+let elemAnimation3 = document.querySelector('.test_title3');
+let elemAnimation4 = document.querySelector('.test_title4');
+let elemAnimation5 = document.querySelector('.test_title5');
+
+
+
+
+    $(window).scroll(function(){
+        let el1 = elemAnimation1.getBoundingClientRect().y;
+        let el2 = elemAnimation2.getBoundingClientRect().y;
+        let el3 = elemAnimation3.getBoundingClientRect().y;
+        let el4 = elemAnimation4.getBoundingClientRect().y;
+        let el5 = elemAnimation5.getBoundingClientRect().y;
+        if (p >= el1 && n <= el1) {
+            // console.log(123);
+            $(elemAnimation1).addClass('element-show');
+        }
+        else {
+            $(elemAnimation1).removeClass('element-show');
+        }
+        if (p >= el2 && n <= el2) {
+            // console.log(123);
+            $(elemAnimation2).addClass('element-show');
+        }
+        else {
+            $(elemAnimation2).removeClass('element-show');
+        }
+        if (p >= el3 && n <= el3) {
+            // console.log(123);
+            $(elemAnimation3).addClass('element-show');
+        }
+        else {
+            $(elemAnimation3).removeClass('element-show');
+        }
+        if (p >= el4 && n <= el4) {
+            // console.log(123);
+            $(elemAnimation4).addClass('element-show');
+        }
+        else {
+            $(elemAnimation4).removeClass('element-show');
+        }
+        if (p >= el5 && n <= el5) {
+            // console.log(123);
+            $(elemAnimation5).addClass('element-show');
+        }
+        else {
+            $(elemAnimation5).removeClass('element-show');
+        }
     });
-  }
-  
-  let options = {
-    threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
--- src/blocks/modules/imagane/imagane.js
+++ src/blocks/modules/imagane/imagane.js
@@ -1,18 +1,28 @@
 AOS.init();
 
-var time = 2;
-  $('.number').each(function() {
-          var
-            i = 1,
-            num = $(this).data('num'),
-            step = 1000 * time / num,
-            that = $(this),
-            int = setInterval(function() {
-              if (i <= num) {
-                that.html(i);
-              } else {
-                clearInterval(int);
-              }
-              i++;
-            }, step);
+var time = 2, 
+iscroll = false,
+idTitle = $('#imagane-title_govno'); // (1)
+
+$(window).scroll(function(){
+  if (window.pageYOffset >= $(idTitle).offset().top) { // (2)
+    if (!iscroll){
+        $('.number').each(function() {
+              var
+                i = 1,
+                num = $(this).data('num'),
+                step = 1000 * time / num,
+                that = $(this),
+                int = setInterval(function() {
+                  if (i <= num) {
+                    that.html(i);
+                  } else {
+                    clearInterval(int);
+                  }
+                  i++;
+                }, step);
+      });
+      iscroll = true;
+      } 
+  }
 });
